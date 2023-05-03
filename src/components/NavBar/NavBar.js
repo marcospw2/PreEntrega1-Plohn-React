6100f94782cb48878e9c222296801d587a0b8b4c
Marcos Plohn <marcospw2@gmail.com>
2023-05-03 15:14:30 -0300

    Pre-entrega1
    1er entrega react

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h3>Ecommerce</h3>
+            <div>
+                <button>Celulares</button>
+                <button>Tablets</button>
+                <button>Notebooks</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar  
---
+++ src/prueba.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>Mi nuevo componente</h1>
+        </div>
+    );
+}
+
+export default MyComponent;
